95a3e16efce5f02f4ae3e054abab6f14986c3bdd
Danisaur117 <danisaur@gmail.com>
2019-05-01 17:36:18 +0200

    Added PyContactAgenda.py script. It will contain the main script

---
+++ PyContactAgenda.py
@@ -0,0 +1,17 @@
+#!C:\Program Files\Python37\python.exe
+
+#
+# FICHERO: PyContactAgenda.py
+#
+# AUTOR:
+#     - Daniel Belmonte Urbano
+#
+# OBSERVACIONES:
+#	- Este archivo contiene la implementación de la vista de una Agenda de Contactos implementada
+#  	  en Python siguiendo el modelo MVC
+
+def main():
+	print("Hola mundo")
+
+if __name__ == "__main__":
+    main()
